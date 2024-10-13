c22b1e6365854a5fccf10960010c94cc778c7813
sanket <sanketj24.rts@gmail.com>
2024-10-13 13:00:47 +0530

    project created to learn task schedular

---
+++ Task Schedular Spring Boot/spring-boot-task-scheduling/src/test/java/com/shedular/spring_boot_task_scheduling/SpringBootTaskSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shedular.spring_boot_task_scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
